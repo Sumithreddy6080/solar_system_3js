30101319036c352e7c0218a1f323da38e1c31d9f
sumithreddy6080 <sumithreddy.6080@gmail.com>
2025-07-16 11:06:44 +0530

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  }
 }
